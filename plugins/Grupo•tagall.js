80e97a325e930e27e4584f3687767ce892719391
RAGNA <ragnabot27@gmail.com>
2024-12-02 15:19:53 -0600

    Add files via upload

--- plugins/Grupo•tagall.js
+++ plugins/Grupo•tagall.js
@@ -1,18 +1,20 @@
-let handler = async(m, { isOwner, isAdmin, conn, text, participants, args, command }) => {
-if (!(isAdmin || isOwner)) {
-global.dfail('admin', m, conn)
-throw false
-}
-let pesan = args.join` `
-let oi = `𝙼𝙴𝙽𝚂𝙰𝙹𝙴: ${pesan}`
-let teks = `❄🎅🏻𝙰𝙲𝚃𝙸𝚅𝙴𝙽𝚂𝙴 𝙱𝙾𝚃𝚂 𝙻𝙴𝚂 𝙷𝙰𝙱𝙻𝙰 𝚂𝙰𝙽𝚃𝙰 𝙲𝙻𝙰𝚄𝚂🎅🏻❄\n${oi}\n\n❄𝙱𝙾𝚃𝚂:❄\n`
-for (let mem of participants) {
-teks += `❄ @${mem.id.split('@')[0]}\n`}
-teks += `RagnaBot`
-conn.sendMessage(m.chat, { text: teks, mentions: participants.map(a => a.id) } )
-}
-handler.command = /^(tagall|invocar|invocacion|todos|invocación)$/i
-handler.admin = true
-handler.group = true
-handler.botAdmin = true
-export default handler
+const handler = async (m, {isOwner, isAdmin, conn, text, participants, args, command, usedPrefix}) => {
+  if (usedPrefix == 'a' || usedPrefix == 'A') return;
+  if (!(isAdmin || isOwner)) {
+    global.dfail('admin', m, conn);
+    return;
+  }
+  const pesan = args.join` `;
+  const colombia = `ᴍᴇɴꜱᴀᴊᴇ: ${pesan}`;
+  let teks = ` ᴀᴄᴛɪᴠᴇɴꜱᴇ ᴀʟᴠ ɪɴꜱᴇʀᴠɪʙʟᴇꜱ\n${colombia}\n\n ᴛᴀɢꜱ: \n`;
+  for (const mem of participants) {
+    teks += `🔥 @${mem.id.split('@')[0]}\n`;
+  }
+  conn.sendMessage(m.chat, {text: teks, mentions: participants.map((a) => a.id)} );
+};
+handler.help = ['todos *<mesaje>*', 'invocar *<mesaje>*'];
+handler.tags = ['grupo'];
+handler.command = ['todos', 'invocar'];
+handler.admin = true;
+handler.group = true;
+export default handler;
